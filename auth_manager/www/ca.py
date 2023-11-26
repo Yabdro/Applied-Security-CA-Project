617025aab4aa1864ab3445fd35d78f1507fdc410
yabdro <hafez435@gmail.com>
2023-11-26 21:31:59 +0100

    certificate revocation function

--- auth_manager/www/ca.py
+++ auth_manager/www/ca.py
@@ -10,6 +10,9 @@ import subprocess
 import os
 from www.models import Users
 
+from datetime import datetime
+
+
 PRE_ISSUED_CERTS = 2
 COUNTRY = "CH"
 PROVINCE = "Zurich"
@@ -103,7 +106,6 @@ def issue_new_certificate(user: Users) -> bytes:
 
 
 
-
 def verify_certificate(client_cert: bytes):
 
     try:
@@ -125,7 +127,67 @@ def verify_certificate(client_cert: bytes):
     except Exception as e:
         print(e)
         return None
-    
+
+def revoke_cert(client_cert: bytes):
+    try: 
+        #First check that certificate is valid before revoking it 
+        verify_certificate(client_cert)
+
+        ca_root_PATH =  "/var/www/auth_manager/ssl/CA"
+        ca_cert_PATH = ca_root_PATH + "/cacert.pem"
+        ca_key_PATH  = ca_root_PATH + "/cakey.pem"
+
+        ca_cert = crypto.load_certificate(FILETYPE_PEM, open(ca_cert_PATH, "rb").read())  
+        ca_key = crypto.load_privatekey(FILETYPE_PEM, open(ca_key_PATH, "rb").read())  
+
+        cert = crypto.load_certificate(FILETYPE_PEM, client_cert)
+
+        #Get current date andd format the date as "YYYYMMDDHHMMSSZ"
+        current_date = datetime.utcnow()
+        formatted_date = current_date.strftime("%Y%m%d%H%M%SZ").encode()
+
+        crl = None 
+
+        crl_file_PATH = ca_root_PATH + "/crl.pem" 
+
+
+        #TODO security risk of open? 
+
+        #Load or create CRL 
+        try:
+            crl = crypto.load_crl(crypto.FILETYPE_PEM, open(crl_file_PATH, "rb").read())
+        except crypto.Error:
+            crl = crypto.CRL()
+        
+        # Create a revoked certificate entry
+        revoked = crypto.Revoked()
+
+        #Set date of revocation 
+        revoked.set_rev_date(formatted_date) 
+
+        #Set serial number of revocation  
+        revoked.set_serial(hex(cert.get_serial_number())[2:]) #TODO  double check 
+
+        #Add revocation entry to CRL object 
+        crl.add_revoked(revoked)
+
+        crl.set_version(1)
+        crl.set_lastUpdate(formatted_date)  
+        crl.set_nextUpdate(formatted_date) 
+        crl.set_issuer(ca_cert.get_subject())
+
+        #Sign the updated CRL object 
+        crl.sign(ca_key, "sha256")
+
+        #Write CRL object to file 
+        with open(crl_file_PATH, "wb") as crl_file:  
+            crl_file.write(crypto.dump_crl(FILETYPE_PEM, crl))
+
+    except Exception as e:
+        print(e)
+        return None
+
+   
 def get_state():
     #TODO: get #revoked certs
     revoked = 0
@@ -137,3 +199,13 @@ def get_state():
         "issued": issued,
         "revoked": revoked
     }
+
+
+
+#Keys in /etc/ssl/CA/privates: 
+#CA key: cakey.pem 
+#auth_manager: auth_manager.key 
+#webserver: webserver.key
+#New certificates in /etc/ssl/CA/newcerts
+#auth_manager: 01.pem 
+#webserver: 02.pem
